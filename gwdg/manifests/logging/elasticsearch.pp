9c6895d21da85516863241d3992cfca00083ce07
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2014-02-18 17:02:09 +0100

    Fixed top level module class references

--- gwdg/manifests/logging/elasticsearch.pp
+++ gwdg/manifests/logging/elasticsearch.pp
@@ -11,13 +11,13 @@ class gwdg::logging::elasticsearch(
   $facter_public_interface  = regsubst("ipaddress_${public_interface}",         '\.', '_')
   $node_public_ip           = inline_template('<%= scope.lookupvar(facter_public_interface) %>')
 
-  sysctl::value { "fs.file-max": value => "65536"}
+#  sysctl::value { "fs.file-max": value => "65536"}
 
   # Setup java
-  class { 'java': }
+  class { '::java': }
 
   # Setup elasticsearch
-  class { 'elasticsearch':
+  class { '::elasticsearch':
     package_url => "https://download.elasticsearch.org/elasticsearch/elasticsearch/${package}",
     config      => {
       'cluster.name'                                    => 'es-cluster',
