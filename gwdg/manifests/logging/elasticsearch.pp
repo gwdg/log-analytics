5a43c79f417ddcd03a2a86318e447add56bb1ffe
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2015-02-14 20:47:24 +0100

    Use current version of elasticsearch + define an elasticsearch instance

--- gwdg/manifests/logging/elasticsearch.pp
+++ gwdg/manifests/logging/elasticsearch.pp
@@ -1,27 +1,45 @@
 #
 class gwdg::logging::elasticsearch(
-  $package            = 'elasticsearch-1.0.0.deb',
+  $package            = 'elasticsearch-1.4.3.deb',
   $public_interface   = 'eth4',
 ){
   
   include gwdg::logging::base
 
-#  sysctl::value { "fs.file-max": value => "65536"}
+  sysctl::value { "fs.file-max": value => "65536"}
 
   # Setup java
   class { '::java': }
 
-  # Setup elasticsearch
+  # Setup elasticsearch base
   class { '::elasticsearch':
-    package_url => "https://download.elasticsearch.org/elasticsearch/elasticsearch/${package}",
-    config      => {
+    package_url       => "https://download.elasticsearch.org/elasticsearch/elasticsearch/${package}",
+    status            => 'enabled',
+    ensure            => 'present',
+#    service_provider  => 'init',  
+  }
+
+  # Setup elasticsearch instance
+  elasticsearch::instance { 'es-01':
+    
+    status  => 'enabled',
+    ensure  => 'present',
+    
+    # Configuration hash
+    config => { 
       'cluster.name'                                    => 'es-cluster',
       'cluster.routing.allocation.awareness.attributes' => 'rack',
       'node.name'                                       => $hostname,
       'network.host'                                    => $gwdg::logging::base::node_public_ip,
 #      'path.logs'                                       => '/var/log/elasticsearch',
 #      'path.data'                                       => '/var/lib/elasticsearch',
-    }
+    },
+    
+    # Init defaults hash
+#   init_defaults => { },
+
+    # Data directory
+#    datadir => [ ],
   }
 
 }
