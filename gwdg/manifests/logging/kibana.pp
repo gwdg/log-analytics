c343e029feec1aeb5aaa3f34bbfb09590170e5e7
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2015-02-23 17:00:38 +0100

    Allow to set kibana git version

--- gwdg/manifests/logging/kibana.pp
+++ gwdg/manifests/logging/kibana.pp
@@ -9,6 +9,7 @@ class gwdg::logging::kibana(
   class { 'kibana3':
     config_es_server   => hiera('elasticsearch::host'),
     config_es_port     => hiera('elasticsearch::port'),
+    k3_release         => hiera('kibana::git_version'),
  #   config_es_protocol => 'http',
   }
 }
